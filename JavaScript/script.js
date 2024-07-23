88ce633899d8b90ec114f4e95e521d6ad2c390da
Vishnupriyan17 <vishnukodees@gmail.com>
2024-07-23 17:59:44 +0530

    CSS and JavaScript

---
+++ JavaScript/script.js
@@ -0,0 +1,19 @@
+function myfunction() {
+    document.write("<b>Example for JavaScript Function");
+    }
+    function addition(a,b){
+        let sum = a+b;
+        document.write("<b>Addition: "+sum+"</b>");
+    }
+    function subtraction(a,b){
+        let sum = a+b;
+        document.write("<b>Addition: "+sum+"</b>");
+    }
+    function multiplication(a,b){
+        let sum = a+b;
+        document.write("<b>Addition: "+sum+"</b>");
+    }
+    function division(a,b){
+        let sum = a+b;
+        document.write("<b>Addition: "+sum+"</b>");
+    }
