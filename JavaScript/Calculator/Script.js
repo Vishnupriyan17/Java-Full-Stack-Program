1d96f6861cee6b4c4d432f781fcf1d51baabc4a6
Vishnupriyan17 <vishnukodees@gmail.com>
2024-07-24 12:36:17 +0530

    JS Assignments'

---
+++ JavaScript/Calculator/Script.js
@@ -0,0 +1,18 @@
+var a='';
+function cleardisplay(){
+     a="0";
+    document.getElementById("display").innerHTML=a;
+}
+function val(num){
+    a = a+num;
+    document.getElementById("display").innerHTML=a;
+}
+function back(){
+    a =(a.slice(0,-1));
+    document.getElementById("display").innerHTML=a;
+}
+function qual(){
+    document.getElementById("display").innerHTML=eval(a);
+    a='';
+
+}
