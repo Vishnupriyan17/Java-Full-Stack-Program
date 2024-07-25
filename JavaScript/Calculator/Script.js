490bedf68d2fe1279db0b4099bb8223076062b35
Vishnupriyan17 <vishnukodees@gmail.com>
2024-07-25 10:14:54 +0530

    CSS assignments

--- JavaScript/Calculator/Script.js
+++ JavaScript/Calculator/Script.js
@@ -1,19 +1,133 @@
 var a='';
+var index = 0;
+var operator = "";
 function cleardisplay(){
      a="0";
     document.getElementById("display").innerHTML=a;
 }
 function val(num){
+
     a = a+num;
     document.getElementById("display").innerHTML=a;
-}
+    
+    
+
+       
+    for(i = 0;i<a.length;i++){
+        if(a.charAt(i)== "+"){
+            operator = a.charAt(i);
+            index = i;
+        }
+        else if(a.charAt(i)== "-"){
+            operator = a.charAt(i);
+            index = i;
+        }
+        else if(a.charAt(i)== "*"){
+            operator = a.charAt(i);
+            index = i;
+        }
+        else if(a.charAt(i)== "/"){
+            operator = a.charAt(i);
+            index = i;
+        }
+        else if(a.charAt(i)== "%"){
+            operator = a.charAt(i);
+            index = i;
+        }
+    }
+    var n1 = parseInt(a.substring(0,index));
+    var n2 = parseInt(a.substring(index+1,a.length-1));
+
+
+    // switch(operator)
+    // {
+    //     case "+":
+    //     (document.getElementById('display').innerHTML=(n1+n2));
+    //     a = '';
+    //     break;
+
+    //     case "-":
+    //     (document.getElementById('display').innerHTML=(n1-n2));
+    //     a = '';
+    //     break;
+
+    //     case "*":
+    //     (document.getElementById('display').innerHTML=(n1*n2));
+    //     a = '';
+    //     break;
+
+    //     case "/":
+    //     (document.getElementById('display').innerHTML=(n1/n2));
+    //     a = '';
+    //     break;
+
+    //     case "%":
+    //     (document.getElementById('display').innerHTML=(n1%n2));
+    //     a = '';
+    //     break;
+    // }
+    }
+
 function back(){
     a =(a.slice(0,-1));
     document.getElementById("display").innerHTML=a;
 }
-function qual(){
-    document.getElementById("display").innerHTML=eval(a);
-    a='';
+function equal(n1,n2){
+    // let s = a.charAt(1);
+    // let n1 = parseInt(a.charAt(0));
+    // let n2 = parseInt(a.charAt(2));
+    // var index = 0;
+    // for(i = 0;i<a.length;i++){
+    //     if(a.charAt(i)== "+"){
+    //         operator = a.charAt(i);
+    //         index = i;
+    //     }
+    //     else if(a.charAt(i)== "-"){
+    //         operator = a.charAt(i);
+    //         index = i;
+    //     }
+    //     else if(a.charAt(i)== "*"){
+    //         operator = a.charAt(i);
+    //         index = i;
+    //     }
+    //     else if(a.charAt(i)== "/"){
+    //         operator = a.charAt(i);
+    //         index = i;
+    //     }
+    //     else if(a.charAt(i)== "%"){
+    //         operator = a.charAt(i);
+    //         index = i;
+    //     }
+    // }
+    // var n1 = parseInt(a.substring(0,index));
+    // var n2 = parseInt(a.substring(index+1,a.length-1));
 
+    switch(operator)
+    {
+        case "+":
+        (document.getElementById('display').innerHTML=(n1+n2));
+        a = '';
+        break;
+
+        case "-":
+        (document.getElementById('display').innerHTML=(n1-n2));
+        a = '';
+        break;
+
+        case "*":
+        (document.getElementById('display').innerHTML=(n1*n2));
+        a = '';
+        break;
+
+        case "/":
+        (document.getElementById('display').innerHTML=(n1/n2));
+        a = '';
+        break;
+
+        case "%":
+        (document.getElementById('display').innerHTML=(n1%n2));
+        a = '';
+        break;
+    }
 }
 
